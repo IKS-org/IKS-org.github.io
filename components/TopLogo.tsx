dc7c887053321df7983c05dad621d81b4c437801
sweshelo <sweshelo@dmats97.xyz>
2022-02-17 19:26:35 +0900

    [fix] static htmlではImageが利用不可

--- components/TopLogo.tsx
+++ components/TopLogo.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image'
 import style from '../styles/Home.module.css'
 
 export const TopLogo = () => {
   return(
     <div className={style.main} id={style.logo}>
       <div className={style.image_wrapper}>
-        <Image src="/icon.png" alt="turningLogo" width={'300px'} height={'300px'} id={style.spiningLogoImg}/>
+        <img src="/icon.png" alt="turningLogo" width={'300px'} height={'300px'} id={style.spiningLogoImg}/>
       </div>
       <h1>IKS - 移動の個別化推進団体</h1>
       <p>
