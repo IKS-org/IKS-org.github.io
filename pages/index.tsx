ce22c16b0466457c58577e530e654744c4ce295e
sweshelo <sweshelo@gmail.com>
2022-02-17 19:05:19 +0900

    [update] TopLogoコンポーネント化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import style from '../styles/Home.module.css'
+import { TopLogo } from '../components/TopLogo'
 
 const Home: NextPage = () => {
   return (
@@ -10,15 +9,7 @@ const Home: NextPage = () => {
         <title>IKS-org</title>
         <link rel="icon" href="icon.png" />
       </Head>
-      <div className={style.main} id={style.logo}>
-        <div className={style.image_wrapper}>
-          <Image src="/icon.png" alt="turningLogo" width={'300px'} height={'300px'} id={style.spiningLogoImg}/>
-        </div>
-        <h1>IKS - 移動の個別化推進団体</h1>
-        <p>
-          We are developing tools to assist Gears against The Dasher organization&apos;s humanoid &apos;DENCO(H)&apos;.
-        </p>
-      </div>
+      <TopLogo />
     </>
   )
 }
