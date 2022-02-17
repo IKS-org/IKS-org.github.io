d6533b10f5aaa042250e211087ec6be18e59ee09
sweshelo <sweshelo@dmats97.xyz>
2022-02-17 18:58:19 +0900

    [update] TOPページ ロゴとテキストを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import style from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -9,7 +10,10 @@ const Home: NextPage = () => {
         <title>IKS-org</title>
         <link rel="icon" href="icon.png" />
       </Head>
-      <div className={style.main}>
+      <div className={style.main} id={style.logo}>
+        <div className={style.image_wrapper}>
+          <Image src="/icon.png" alt="turningLogo" width={'300px'} height={'300px'} id={style.spiningLogoImg}/>
+        </div>
         <h1>IKS - 移動の個別化推進団体</h1>
         <p>
           We are developing tools to assist Gears against The Dasher organization&apos;s humanoid &apos;DENCO(H)&apos;.
