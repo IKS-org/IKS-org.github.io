a5f32059198554845d7787c5f6458f8c5152a86c
sweshelo <sweshelo@dmats97.xyz>
2022-02-19 17:24:16 +0900

    [fix] コンポーネント分割

--- components/TopLogo.tsx
+++ components/TopLogo.tsx
@@ -1,14 +1,15 @@
-import style from '../styles/Home.module.css'
+import style from './TopLogo.module.scss'
 
 export const TopLogo = () => {
   return(
-    <div className={style.main} id={style.logo}>
-      <div className={style.image_wrapper}>
+    <div className={style.topLogo}>
+      <div className={style.topLogo_image_wrapper}>
         <img src="/icon.png" alt="turningLogo" width={'300px'} height={'300px'} id={style.spiningLogoImg}/>
       </div>
-      <h1>IKS - 移動の個別化推進団体</h1>
+      <h1>IKS</h1>
+      <h2>移動の個別化推進団体</h2>
       <p>
-        We are developing tools to assist Gears against The Dasher organization&apos;s humanoid &apos;DENCO(H)&apos;.
+        We are developing tools to assist Gears<br className={style.sp}/>against The Dasher organization&apos;s humanoid &apos;DENCO(H)&apos;.
       </p>
     </div>
   )
