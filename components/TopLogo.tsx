1bc152d6fd77408852e89edf432828b30e8ba0b1
sweshelo <sweshelo@gmail.com>
2022-02-20 00:24:09 +0900

    [refactor] コード整形

--- components/TopLogo.tsx
+++ components/TopLogo.tsx
@@ -1,17 +1,24 @@
-import style from './TopLogo.module.scss'
+import style from "./TopLogo.module.scss";
 
 export const TopLogo = () => {
-  return(
+  return (
     <div className={style.topLogo}>
       <div className={style.topLogo_image_wrapper}>
-        <img src="/icon.png" alt="turningLogo" width={'300px'} height={'300px'} id={style.spiningLogoImg}/>
+        <img
+          src="/icon.png"
+          alt="turningLogo"
+          width={"300px"}
+          height={"300px"}
+          id={style.spiningLogoImg}
+        />
       </div>
       <h1>IKS</h1>
       <h2>移動の個別化推進団体</h2>
       <p>
-        We are developing tools to assist Gears<br className={style.sp}/>against The Dasher organization&apos;s humanoid &apos;DENCO(H)&apos;.
+        We are developing tools to assist Gears
+        <br className={style.sp} />
+        against The Dasher organization&apos;s humanoid &apos;DENCO(H)&apos;.
       </p>
     </div>
-  )
-}
-
+  );
+};
